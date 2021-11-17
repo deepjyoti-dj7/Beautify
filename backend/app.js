318023476fca220792020f510c4a4577ef5252b4
DEEPJYOTI <deepjyoti0072@gmail.com>
2021-11-17 20:21:56 +0530

    Till CRUD operation done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
 const app = express();
 
-export default app;
+app.use(express.json());
+
+//Route imports
+const product = require("./routes/productRoute");
+
+app.use("/api/v1", product);
+
+module.exports = app;
